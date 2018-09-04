b8f280c23fa3f0cb40cd9a58dcf9f6ef76d158c1
semioys <semioys@gmail.com>
2018-09-04 19:40:22 +0300

    Cyrillic regular font added, fixed color in progress bars

--- src/js/main.js
+++ src/js/main.js
@@ -8,21 +8,25 @@
 
     if (bodyScroll > 1500) {
       $("#bar1").barfiller({
+        barColor: '#1abc9c',
         // duration in ms
         duration: 800
       });
 
       $("#bar2").barfiller({
+        barColor: '#1abc9c',
         // duration in ms
         duration: 800
       });
 
       $("#bar3").barfiller({
+        barColor: '#1abc9c',
         // duration in ms
         duration: 1200
       });
 
       $("#bar4").barfiller({
+        barColor: '#1abc9c',
         // duration in ms
         duration: 1200
       });
